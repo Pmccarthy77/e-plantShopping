d2e9fc872a8e276c3b9bdfcbec092b1928a75bb9
Powell McCarthy <pgmccarthy77@gmail.com>
2024-07-24 15:59:49 -0400

    cartSlice and ProductList changes

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,13 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
+
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const handleAddToCart = (product) => {
+            dispatch(addItem(product));
+            setAddedToCart((prevState) => ({
+               ...prevState,
+               [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+             }));
+          };
     },
     removeItem: (state, action) => {
     },
